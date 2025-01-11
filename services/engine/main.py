3edaa7ad1d26768c4de9ae2777a625f5deb3afbc
Adrián Martínez Martínez <adrimrtnz@gmail.com>
2025-01-11 16:34:13 +0100

    pruebas parser

--- services/engine/main.py
+++ services/engine/main.py
@@ -11,13 +11,22 @@ if __name__ == '__main__':
 
     doc = minidom.parse(f'../../scenes/{input_scene}.{input_format}')
     config = doc.getElementsByTagName('config')[0]
+    element_nodes = [node for node in config.childNodes if node.nodeType == minidom.Node.ELEMENT_NODE]
 
-    for membrane in config.getElementsByTagName('membrane'):
-        m_id  = membrane.getAttribute("id")
-        m_mul = membrane.getAttribute("m")
-        m_cap = membrane.getAttribute("capacity")
 
-        print(f'Membrane id={m_id} | multiplicity={m_mul} | capacity={m_cap}')
+    def iterate_node(node, parent=None):
+        for hijo in node.childNodes:
+            if hijo.nodeType == minidom.Node.ELEMENT_NODE:
+                parent_name = parent.nodeName if parent else ''
+                m_id  = hijo.getAttribute("id")
+                m_mul = hijo.getAttribute("m")
+                m_cap = hijo.getAttribute("capacity")
+                print(f'Nodo: {hijo.nodeName} | Padre: {parent_name}')
+                print(f'    -Membrane id={m_id} | multiplicity={m_mul} | capacity={m_cap}')
 
+                if hijo.hasChildNodes():
+                    iterate_node(hijo, hijo)
+
+    iterate_node(config)
 
 
