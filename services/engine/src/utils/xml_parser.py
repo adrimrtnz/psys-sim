ce232c769373d83194ab50dca2eed7001f02b5de
Adrián Martínez Martínez <adrimrtnz@gmail.com>
2025-02-28 17:24:14 +0100

    name refactoring in xml_parser

--- services/engine/src/utils/xml_parser.py
+++ services/engine/src/utils/xml_parser.py
@@ -1,23 +1,23 @@
 from xml.dom import minidom
 
 class XMLInputParser:
-    def __init__(self, path):
-        self._path = path
-        doc = minidom.parse(f'../../scenes/{path}.xml')
+    def __init__(self, scene):
+        self._scene = scene
+        doc = minidom.parse(f'../../scenes/{scene}.xml')
         self._root = doc.getElementsByTagName('config')[0]
 
     def iterate_node(self, node, parent=None):
-        for hijo in node.childNodes:
-            if hijo.nodeType == minidom.Node.ELEMENT_NODE:
+        for child in node.childNodes:
+            if child.nodeType == minidom.Node.ELEMENT_NODE:
                 parent_name = parent.nodeName if parent else ''
-                m_id  = hijo.getAttribute("id")
-                m_mul = hijo.getAttribute("m")
-                m_cap = hijo.getAttribute("capacity")
-                print(f'Nodo: {hijo.nodeName} | Padre: {parent_name}')
+                m_id  = child.getAttribute("id")
+                m_mul = child.getAttribute("m")
+                m_cap = child.getAttribute("capacity")
+                print(f'Nodo: {child.nodeName} | Padre: {parent_name}')
                 print(f'    -Membrane id={m_id} | multiplicity={m_mul} | capacity={m_cap}')
 
-                if hijo.hasChildNodes():
-                    self.iterate_node(hijo, hijo)
+                if child.hasChildNodes():
+                    self.iterate_node(child, child)
 
     def parse(self):
         self.iterate_node(self._root)
