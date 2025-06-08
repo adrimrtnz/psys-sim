d95d83760f3b8afdc62f4dc57b66f7a070314a07
Adrián Martínez Martínez <adrimrtnz@gmail.com>
2025-06-08 14:03:45 +0200

    fix typos

--- services/engine/src/classes/move_code_helper.py
+++ services/engine/src/classes/move_code_helper.py
@@ -1,26 +1,26 @@
-from src.enums.constants import MoveCodes
+from src.enums.constants import MoveCode
 
 class MoveCodeHelper:
 
     @staticmethod
     def get_move_code(code: str):
         """Get the value of a move description"""
-        move_code = MoveCodes.__members__.get(code, None)
+        move_code = MoveCode.__members__.get(code, None)
         if move_code:
             return move_code.value
         raise ValueError(f'The code {code} is not a valid MoveCode.')
     
     @staticmethod
     def has_son_destination(value: int):
-        return value == MoveCodes.IN.value
+        return value == MoveCode.IN.value
     
     @staticmethod
     def has_brother_destination(value: int):
         valid_destination = [
-            MoveCodes.MEM.value,
-            MoveCodes.MEM_WC.value,
-            MoveCodes.MEM_TRANS.value,
-            MoveCodes.GROUP_TRANS.value,
-            MoveCodes.MEM_W_OB.value,
-            MoveCodes.DMEM.value]
+            MoveCode.MEM.value,
+            MoveCode.MEMWC.value,
+            MoveCode.MEMTRANS.value,
+            MoveCode.GROUP_TRANS.value,
+            MoveCode.MEMwOB.value,
+            MoveCode.DMEM.value]
         return value in valid_destination
