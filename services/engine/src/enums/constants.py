a5cd616a6e6538be1eba346392b7c147f3d82761
Adrián Martínez Martínez <adrimrtnz@gmail.com>
2025-06-14 13:55:31 +0200

    constants docstrings

--- services/engine/src/enums/constants.py
+++ services/engine/src/enums/constants.py
@@ -1,23 +1,38 @@
 from enum import Enum
 
 """
-MoveCodes explanation:
-    HERE: destination is the same region
-    OUT: destination is the parent region
-    IN: destination is a region whithin the current one
-    MEM: destination of elements is other region at same level
-    DISS: dissolve region and remove its elements
-    DISS2: dissolve region and its elements go to the parent
-    MEMWC: marks a movement of one membrane between regions but in the transmitting región take a copy of the membrane
-    MEMTRANS: MEM for plasmid transmision
-    GROUP_TRANS: one region pass a % of some membranes to another region at the same level
-    MEMwOB: move membranes with some specific object inside
-    DMEM: MEM but does de transmition at the moment
+Constants module for membrane computing system.
+
+This module defines enumerations and constants used throughout the membrane
+computing simulation system, including movement codes, scene objects, and
+inference types.
 """
 
-# TODO revisar todos los nombres para que sean más significativos
 
 class MoveCode(Enum):
+    """Enumeration of movement codes for membrane operations.
+    
+    This enum defines the different types of movements and operations that can
+    be performed on membranes and objects within the membrane computing system.
+    
+    Attributes:
+        HERE (int): Destination is the same region.
+        OUT (int): Destination is the parent region.
+        IN (int): Destination is a region within the current one.
+        MEM (int): Destination of elements is other region at same level.
+        DISS (int): Dissolve region and remove its elements.
+        DISS2 (int): Dissolve region and its elements go to the parent.
+        MEMWC (int): Marks a movement of one membrane between regions but in
+            the transmitting region take a copy of the membrane.
+        MEMTRANS (int): MEM operation for plasmid transmission.
+        GROUP_TRANS (int): One region passes a percentage of some membranes
+            to another region at the same level.
+        MEMwOB (int): Move membranes with some specific object inside.
+        DMEM (int): MEM operation but does the transmission at the moment.
+    
+    Note:
+        TODO: Review all names to make them more meaningful.
+    """
     HERE = 1
     OUT = 2
     IN = 3
@@ -32,7 +47,20 @@ class MoveCode(Enum):
     
 
 class SceneObject():
-    """Allow name changes with minor code impact"""
+    """Constants for scene object identifiers.
+    
+    This class provides string constants for different types of objects
+    in the membrane computing scene. It allows name changes with minor
+    code impact throughout the system.
+    
+    Attributes:
+        MEMBRANE (str): Identifier for membrane objects.
+        MEMBRANE_RULE (str): Identifier for membrane rules.
+        OBJECT (str): Identifier for basic objects.
+        OBJECT_RULE (str): Identifier for object rules.
+        RULE_LH (str): Identifier for left-hand side of rules.
+        RULE_RH (str): Identifier for right-hand side of rules.
+    """
     MEMBRANE = 'membrane'
     MEMBRANE_RULE = 'rMM'
     OBJECT = 'BO'
@@ -42,5 +70,14 @@ class SceneObject():
 
 
 class InferenceType():
+    """Constants for inference execution types.
+    
+    This class defines the different types of inference modes available
+    in the membrane computing system.
+    
+    Attributes:
+        MIN_PARALLEL (str): Minimal parallel inference mode.
+        SEQUENTIAL (str): Sequential inference mode.
+    """
     MIN_PARALLEL = 'minpar'
     SEQUENTIAL = 'sequential'
