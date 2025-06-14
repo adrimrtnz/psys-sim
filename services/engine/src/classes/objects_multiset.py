089a22b5eecbab6e766a4f25b6aed06c21cc75b0
Adrián Martínez Martínez <adrimrtnz@gmail.com>
2025-06-14 13:54:47 +0200

    fix remove

--- services/engine/src/classes/objects_multiset.py
+++ services/engine/src/classes/objects_multiset.py
@@ -41,22 +41,15 @@ class ObjectsMultiset(MultiSetInterface):
     def is_empty(self):
         return self.count_subsets() == 0
 
-    def remove(self, obj: str, multiplicity: int = 1) -> bool:
+    def remove(self, obj: str) -> bool:
         if obj is None:
             raise ValueError("ObjectsMultiset.remove -> object cannot be null")
-        if multiplicity < 0:
-            raise ValueError("ObjectsMultiset.remove -> object multiplicity cannot be negative")
-        if multiplicity == 0:
-            return False
+        
         current_multiplicity = self.multiset.get(obj, None)
         if current_multiplicity is None:
             return False
-        if current_multiplicity < multiplicity:
-            return False
-        if current_multiplicity == multiplicity:
-            del self.multiset[obj]
-        else:
-            self.multiset[obj] = self.multiset.get(obj, 0) - multiplicity
+
+        del self.multiset[obj]
         return True
 
     def remove_all(self):
