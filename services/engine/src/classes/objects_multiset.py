b4d67512f1e4f194f04d1767a56b87e82349185d
Adrián Martínez Martínez <adrimrtnz@gmail.com>
2025-07-13 10:59:16 +0200

    changed ObjectsMultiset.count_subsets

--- services/engine/src/classes/objects_multiset.py
+++ services/engine/src/classes/objects_multiset.py
@@ -35,11 +35,41 @@ class ObjectsMultiset(MultiSetInterface):
     def count(self, _object):
         return self._multiset.get(_object, 0)
 
-    def count_subsets(self):
-        return len(self.multiset.keys())
+    def count_subsets(self, other):
+        """
+        Count how many times the ObjectMultiset 'other' is included in self.
+        
+        Args:
+            other
+        
+        Returns:
+            int: number of times that self contains other
+        """
+        if not isinstance(other, self.__class__):
+            return 0
+        
+        # If other.multiset is empty, it is technically included infinitely many times.
+        if not other.multiset:
+            return float('inf')
+            
+        min_quotient = float('inf')
+    
+        for obj, mult in other._multiset.items():
+            self_count = self._multiset.get(obj, 0)
+            if self_count == 0:
+                return 0
+            
+            quotient = self_count // mult
+            min_quotient = min(min_quotient, quotient)
+            
+            # If it is already 0, stop and return
+            if min_quotient == 0:
+                return 0
+        
+        return min_quotient
 
     def is_empty(self):
-        return self.count_subsets() == 0
+        return len(self.multiset.keys()) == 0
 
     def remove(self, obj: str) -> bool:
         if obj is None:
